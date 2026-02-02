ffd49194f916161f136db61178ea0c75f456ff07
Yugen1121 <yugenlimbu777@gmail.com>
2026-02-02 22:26:04 +0000

    initial

---
+++ Yugen/src/main/java/com/Yugen/Yugen/YugenApplication.java
@@ -0,0 +1,13 @@
+package com.Yugen.Yugen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YugenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YugenApplication.class, args);
+	}
+
+}
---
+++ Yugen/src/test/java/com/Yugen/Yugen/YugenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Yugen.Yugen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YugenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
